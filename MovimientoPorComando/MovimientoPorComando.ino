5cce182cd498daf66db29ff213ce3fe4aeda6f34
Juan Pablo Arévalo <jp.arevalo291@uniandes.edu.co>
2018-02-15 17:28:33 -0500

    Cambios para reutilización de código

--- MovimientoPorComando/MovimientoPorComando.ino
+++ MovimientoPorComando/MovimientoPorComando.ino
@@ -9,8 +9,7 @@ int m2direction=4;
 int tiempo = 250;
 int delayP = 1000;
 int Comando_0;
-int poscicionIzquierdo = 0;
-int poscicionDerecho = 0;
+int poscicion = 0;
 /**
  * Configuración inicial, se seleccionan los pines de salida para las ruedas y se hace begin al wire y al serial 
  */
@@ -33,8 +32,8 @@ void loop()
 {
   esperarComando();
   ejecutarMovimientos();
-  leerIzquierdo();
-  leerDerecho();
+  leer(7);
+  leer(8);
 }
 /**
  * Realiza espera activa sobre el serial 
@@ -59,118 +58,57 @@ void esperarComando()
  */
 void ejecutarMovimientos()
 {
-  goForward(tiempo, 255);
- delay(delayP);
- rotateLeft(tiempo, 255);
- delay(delayP);
- rotateRight(tiempo, 255);
- delay(delayP);
- goBackward(tiempo, 255);
- delay(delayP);
-}
-/**
- * Realiza el movimiento hacia adelante, hace escritura digital a partir de los pines impuestos como variables m1direction, m2direction con valor High (forward)
- * Luego escribe analogicamente en los pines de control de velocidad m1speed y m2speed con una velocidad como parametro (pwm)
- * mantiene esa velocidad por una duración puesta como parámetro 
- * escribe en velocidad 0 para detener los motores. 
- */
-void goForward(int duration, int pwm)
-{
- digitalWrite(m1direction,HIGH); // forward
- digitalWrite(m2direction,HIGH); // forward
- analogWrite(m1speed, pwm); // speed
- analogWrite(m2speed, pwm);
- delay(duration);
- analogWrite(m1speed, 0); // speed
- analogWrite(m2speed, 0);
-}
-/**
- * Realiza el movimiento hacia adelante, hace escritura digital a partir de los pines impuestos como variables m1direction, m2direction con valor LOW (backward)
- * Luego escribe analogicamente en los pines de control de velocidad m1speed y m2speed con una velocidad como parametro (pwm)
- * mantiene esa velocidad por una duración puesta como parámetro 
- * escribe en velocidad 0 para detener los motores. 
- */
-void goBackward(int duration, int pwm)
-{
- digitalWrite(m1direction,LOW); // backward
- digitalWrite(m2direction,LOW); // backward
- analogWrite(m1speed, pwm); // speed
- analogWrite(m2speed, pwm);
- delay(duration);
- analogWrite(m1speed, 0); // speed
- analogWrite(m2speed, 0);
-}
-/**
- * Realiza el movimiento hacia la derecha, hace escritura digital a partir de los pines impuestos como variables m1direction, m2direction con valor HIGH en m1 y LOW en m2 (forward)
- * Luego escribe analogicamente en los pines de control de velocidad m1speed y m2speed con una velocidad como parametro (pwm)
- * mantiene esa velocidad por una duración puesta como parámetro 
- * escribe en velocidad 0 para detener los motores. 
- */
-void rotateRight(int duration, int pwm)
-{
- digitalWrite(m1direction,HIGH); // forward
- digitalWrite(m2direction,LOW); // backward
- analogWrite(m1speed, pwm); // speed
- analogWrite(m2speed, pwm);
- delay(duration);
- analogWrite(m1speed, 0); // speed
- analogWrite(m2speed, 0);
-}
-/**
- * Realiza el movimiento hacia la derecha, hace escritura digital a partir de los pines impuestos como variables m1direction, m2direction con valor LOW en m1 y HIGH en m2 (forward)
- * Luego escribe analogicamente en los pines de control de velocidad m1speed y m2speed con una velocidad como parametro (pwm)
- * mantiene esa velocidad por una duración puesta como parámetro 
- * escribe en velocidad 0 para detener los motores. 
- */
-void rotateLeft(int duration, int pwm)
-{
- digitalWrite(m1direction,LOW); // backward
- digitalWrite(m2direction,HIGH); // forward
- analogWrite(m1speed, pwm); // speed
- analogWrite(m2speed, pwm);
- delay(duration);
  
+ avance(tiempo, 255, HIGH, HIGH);
+ delay(delayP);
+ avance(tiempo, 255, LOW, HIGH);
+ delay(delayP);
+ avance(tiempo, 255, HIGH,LOW);
+ delay(delayP);
+ avance(tiempo, 255, LOW,LOW);
+ delay(delayP);
+}
+/**
+ * Realiza el movimiento hacia la derecha, hace escritura digital a partir de los pines impuestos como variables m1direction, m2direction con valor izq en m1 y der en m2 como parámetro 
+ * Luego escribe analogicamente en los pines de control de velocidad m1speed y m2speed con una velocidad como parametro (pwm)
+ * mantiene esa velocidad por una duración puesta como parámetro 
+ * escribe en velocidad 0 para detener los motores. 
+ */
+void avance(int duration, int pwm,int izq, int der)
+{
+ digitalWrite(m1direction,izq); 
+ digitalWrite(m2direction,der); 
+ analogWrite(m1speed, pwm); // speed
+ analogWrite(m2speed, pwm);
+ delay(duration);
  analogWrite(m1speed, 0); // speed
  analogWrite(m2speed, 0);
 }
 /**
- * Se entiende como izquiero el sensor del lado izquierdo con robot mirando hacia el frente
- * Tiene direccion 7
+ * Se entiende como izquiero el sensor del lado izquierdo con robot mirando hacia el frente con dirección 7
+ * Se entiende como derecho el sensor del lado izquierdo con robot mirando hacia el frente con dirección 8
  * realiza la lectura en cada uno de los pines, calcula el ángulo e imprime en el serial
  */
- void leerIzquierdo()
- {
-  // Solicitar dos bytes de 7
+void leer(int pin)
+{
+// Solicitar dos bytes del pin por parámetro
   int d1 = 0;
   int d0 = 0;
-  Wire.requestFrom(7, 2);
+  Wire.requestFrom(pin, 2);
   while (Wire.available())// slave may send less than requested
   { 
     d0 = Wire.read()-'0';
     d1 = Wire.read()-'0';
-    poscicionIzquierdo = d1*10 + d0;
-    Serial.print("Izquierdo: ");
-    Serial.println(poscicionIzquierdo);
-  }  
- }
- /**
- * Se entiende como derecho el sensor del lado izquierdo con robot mirando hacia el frente
- * Tiene direccion 8
- * realiza la lectura en cada uno de los pines, calcula el ángulo e imprime en el serial
- */
- void leerDerecho()
- {
-  // Solicitar dos bytes de 8
-  int d1 = 0;
-  int d0 = 0;
-  Wire.requestFrom(8, 2);
-  while (Wire.available())// slave may send less than requested
-  { 
-    d0 = Wire.read()-'0';
-    d1 = Wire.read()-'0';
-    poscicionDerecho = d1*10 + d0;
-    Serial.print("Derecho: ");
-    Serial.println(poscicionDerecho);
+    poscicion = d1*10 + d0;
+    if (pin == 7)
+    {
+      Serial.print("Izquierdo: ");
+    }
+    if (pin == 8)
+    {
+      Serial.print("Derecho: ");
+    }
+    Serial.println(poscicion);
   }
- }
-
+}
+ 
